1c3a698c99605ee6be3ffde37fa8d832dd9cc49e
Saiham <siaabid5323@gmail.com>
2021-12-28 10:11:17 +0600

    pushed from uni

--- String 1/c.cpp
+++ String 1/c.cpp
@@ -1,24 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define rb rbegin()
+#define re rend()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
 
 ll hash_2d[1001][1001];
-ll hash_pat[101][101];
+ll hash_pat[101];
 ll p=31;
 ll mod = 1e9 + 7;
 ll inverse_p[1001];
 
-ll sub_hash(ll hashi, ll hashj, ll pi)
+int binPow(ll x, ll n)
 {
-
+    if(n==0)    return 1;
+    ll res = binPow(x, n/2);
+    if(n&1)
+        return (res * res * x)%mod;
+    else
+        return (res*res)%mod;
 }
 
-int mmi
+ll sub_hash(ll hashi, ll hashj, ll pi)
+{
+    ll ans=(hashj-hashi)%mod;
+    ll temp=binPow(pi, mod-2);
+    return (ans*temp)%mod;
+}
+
 
 void solve()
 {
-    ll pp;
+    // ll pp;
     int n,m,x,y;
 
     cin>>n>>m;
@@ -31,35 +62,81 @@ void solve()
     for(int i=0;i<x;i++)
         cin>>pat[i];
 
-    for(int i=0;i<n;i++)
-    {
-        inverse_p[i]=mmi(p,i+1);
-    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     inverse_p[i]=mmi(p,i+1);
+    // }
 
     ll ans=0;
+    vll pp(n);
+    pp[0]=1;
+    for (int i = 1; i < n; i++)
+    {
+        pp[i] = (pp[i-1]*p)%mod;
+    }
+    
     for(int i=0;i<n;i++)
     {
-        pp=1;
         for(int j=0;j<m;j++)
         {
-            hash_2d[i+1][j+1]=(hash_2d[i][j]+(s[i][j]-96)*pp)%mod;
-            pp*=p;
+            hash_2d[i+1][j+1]=(hash_2d[i+1][j]+(s[i][j]-96)*pp[j])%mod;
         }
     }
+
     for(int i=0;i<x;i++)
     {
-        pp=1;
-        for(int j=0;j<y;j++)
+        for (int j = 0; j < y; j++)
         {
-            hash_pat[i+1][j+1]=(hash_pat[i][j]+(pat[i][j]-96)*pp)%mod;
-            pp*=p;
+            hash_pat[i+1]+=((pat[i][j]-96)*pp[j])%mod;
         }
     }
-    // match for sub hash
+
+    for (int i = 0; i < n-x; i++)
+    {
+        for (int j = 0; j < m-y; j++)
+        {
+            bool match=true;
+            int k=i;
+            while(match)
+            {
+                ll suha = sub_hash(hash_2d[k][j],hash_2d[k][j+y],pp[j]);
+                cout<<"SUB "<<suha<<endl;
+                if(suha!=hash_pat[k])
+                {
+                    match=false;
+                    break;
+                }
+                k++;
+            }
+            if(match)
+                ans++;
+        }
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            cout<<hash_2d[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+   
+    for (int j = 0; j <= x; j++)
+    {
+        cout<<hash_pat[j]<<endl;
+    }
+
+    cout<<"***  "<<ans<<endl;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+    
+    freopen("in","r",stdin);
+    
     int t;
     cin>>t;
     while(t--)
--- String 1/f.cpp
+++ String 1/f.cpp
@@ -1,6 +1,30 @@
 // knuth morris pratt
-#include<bits/stdc++.h>
+////////////////////////////////////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
 using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define rb rbegin()
+#define re rend()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
 
 int failure[100000];
 void build(string pat, int size)
@@ -26,16 +50,15 @@ void build(string pat, int size)
         }
     }
 }
-
-int kmp(string text, string pat)
+int kmp(string text, string pat, vector<int> v)
 {
     int n, m;
     n=text.size();
     m=pat.size();
     build(pat, m);
 
-    int i, j,cnt;
-    i=j=cnt=0;
+    int i, j, cnt, k;
+    i=j=cnt=0, k=1;
 
     while(true)
     {
@@ -49,8 +72,8 @@ int kmp(string text, string pat)
             j++;
             if(i==m)
             {
-                i=0;
                 cnt++;
+                j=v[k++];
             }
         }
         else
@@ -64,19 +87,32 @@ int kmp(string text, string pat)
     return cnt;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+
     int n,t;
     cin>>n>>t;
     string s="";
+    vi v;
+    v.pb(0);
     for(int i=0;i<n;i++)
     {
-        string ss;cin>>ss;
-        s+=ss+"#";
+        string temp;
+        cin>>temp;
+        s+=temp;
+        v.pb(v.back()+temp.size());
     }
+    // v.pop_back();
     for(int i=0;i<t;i++)
     {
+        int ans=0;
+        string pat;
+        cin>>pat;
+        cout<<kmp(s,pat,v)<<endl;
+    }   
 
-    }
     return 0;
 }
