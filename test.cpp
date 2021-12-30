23c67ca3b26d0cd10a8892a2f53c7e6be57acb45
saiham <siaabid5323@gmail.com>
2021-12-30 19:38:26 +0600

    test

---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<vector<int>> v;
+    int x;
+    cin>>x;
+    v.emplace_back();
+    v[0].push_back(x);
+    for(int i=1;i<n;i++)
+    {
+        cin>>x;
+        bool flag=true;
+        for(auto &k:v)
+        {
+            if(x>k.back())
+            {
+                k.push_back(x);
+                flag=false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            v.emplace_back();
+            v.back().push_back(x);
+        }
+    }
+    for(auto &k:v)
+    {
+        for(auto &j:k)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
