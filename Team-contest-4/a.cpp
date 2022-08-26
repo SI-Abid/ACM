9293738c558b1e534ed880dadeb9baf5682d9a28
Abid <siaabid5323@gmail.com>
2022-08-26 12:51:12 +0600

    dp graph

--- Team-contest-4/a.cpp
+++ Team-contest-4/a.cpp
@@ -1,78 +1,52 @@
 #include<bits/stdc++.h>
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+#define ed '\n'
+#define pi 2 * acos(0.0)
+#define mod 1000000007
+#define Case(x) cout<<"Case "<<x<<": ";
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0)
 using namespace std;
+ll cost[105];
+map< ll, pair< ll,ll > >dis;
+vector< pair< ll,ll > >g[105];
 
-void dijkstra(vector<vector<pair<int, int>>> &graph, pair<int,int> src, int n)
-{
-    priority_queue<pair<int,int>> pq;
-    pq.push({0, src});
-    vector<int> dist(n, INT_MAX);
-    dist[src.first] = 0;
-    while (!pq.empty())
-    {
-        pair<int, int> top = pq.top();
-        pq.pop();
-        int u = top.second.first;
-        int v = top.second.second;
-        if (dist[u] < top.first)
-            continue;
-        for (auto it : graph[u])
-        {
-            if (dist[it.first] > dist[u] + it.second)
-            {
-                dist[it.first] = dist[u] + it.second;
-                pq.push({dist[it.first], {it.first, v}});
-            }
-        }
-    }
-}
 
 int main()
 {
-    pair<int,int> src, dst;
-    cin>>src.first>>src.second;
-    cin>>dst.first>>dst.second;
-    int b,c0;
-    cin>>b>>c0;
-    int t;cin>>t;
-    vector<int> modes(t);
-    for(int i=0;i<t;i++)
+    FastRead;
+    ll t,n,m,i,j,x,y,x1,y1,maxDis,c0;
+    cin>>x>>y>>x1>>y1>>maxDis>>c0;>>t;
+    for(i=0;i<t;i++)
     {
-        cin>>modes[i];
+        cin>>cost[i];
+
     }
-    vector<pair<int,int>> station;
-    int n;cin>>n;
-    vector<vector<pair<int,int>>> link(n+1);
-    for(int i=0;i<n;i++)
-    {
-        int x,y;cin>>x>>y;
-        station.pb({x,y});
-        int num;cin>>num;
-        for(int j=0;j<num;j++)
-        {
-            int to,mode;
-            cin>>to<<mode;
-            link[i].pb({to-1,modes[mode-1]});
-            link[to-1].pb({i,modes[mode-1]});
+    g[0].pb({n+1,c0});
+    g[n+1].pb({0,c0});
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cin>>a>>b;
+        dis[i] = {a,b};
+        g[0].pb({i,c0});
+        g[i].pb({0,c0});
+        g[n+1].pb({i,c0});
+        g[i].pb({n+1,c0});
+        cin>>k;
+        for(j=0;j<k;j++){
+            cin>>c>>d;
+            g[i].pb({c,cost[d-1]});
+            g[c].pb({i,cost[d-1]});
         }
     }
-    pair<int,int> src2,dst2;
-    int src_d,dst_d;
-    src_d=dst_d=INT_MAX;
-    for(int i=0;i<n;i++)
-    {
-        int d=abs(station[i].first-src.first)+abs(station[i].second-src.second);
-        if(d<src_d)
-        {
-            src2=station[i];
-            src_d=d;
-        }
-        d = abs(station[i].first-dst.first)+abs(station[i].second-dst.second);
-        if(d<dst_d)
-        {
-            dst2=station[i];
-            dst_d=d;
-        }
+
     }
 
     return 0;
 }
+
+
+
+
